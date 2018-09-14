7fc319bd1e23297c6977cd72336b7a0fb3162393
Anuj Yadav <anujy647@gmail.com>
2018-09-14 16:38:02 +0530

    generate msg utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port  = process.env.PORT || 3000;
 var app = express();
@@ -13,22 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New User connected');
 
+    socket.emit('newMsg',generateMessage('admin','welcom to the chat app'));
+
+    socket.broadcast.emit('newMsg',generateMessage('admin', 'welcom to the chat app'));
+
     socket.on('createMsg', (msg) => {
         console.log('msg created', msg);
-    });
-
-    socket.emit('newMsg',{
-        to:'c@gm.com',
-        text:''
+        io.emit('newMsg',generateMessage(msg.from,msg.text))
     });
 
     socket.on('disconnect', (socket) => {
         console.log("Disconnected from client");
     });
 });
-
-
-
 
 server.listen(port,()=>{
     console.log(`Starting at the post ${port}`);
