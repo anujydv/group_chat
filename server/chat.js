6e296ca9da08a3c0bbb38c777f0107d089c13391
Anuj Yadav <anujy647@gmail.com>
2018-10-08 03:36:18 +0530

    completed

--- server/chat.js
+++ server/chat.js
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
     var user = users.getUser(socket.id);
 
     if (user) {
-      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
     }
   });
 
